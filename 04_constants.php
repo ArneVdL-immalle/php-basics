4c35b622dccc8c9e97b40f47bfc61246cf6e60d7
Arne <arne.vdlangenbergh@immalle.be>
2017-02-20 09:27:13 +0100

    oef 4

--- 04_constants.php
+++ 04_constants.php
@@ -3,7 +3,13 @@
 define("USERNAME", "joske");
 
 echo USERNAME;
-
+echo "<br>"; 
 // definieer ook een constante PASSWORD en SERVERNAME
+define("PASSWORD", "mypass");
 
+echo PASSWORD;
+echo "<br>";
+define ("SERVERNAME", "myserver");
+
+echo SERVERNAME;
 ?>
