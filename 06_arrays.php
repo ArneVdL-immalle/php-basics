71a24a3a47f99b9800abf94c1074ca89a23dc037
Arne <arne.vdlangenbergh@immalle.be>
2017-02-20 09:55:50 +0100

    oef 6

--- 06_arrays.php
+++ 06_arrays.php
@@ -1,17 +1,23 @@
 <?php
 
 // Maak een Array met 3 namen van medeleerlingen.
-
+$arr = array("Axel","Kenzo","Daan");
 
 // Itereer er over met een for-loop. Print ze af in een HTML ongenummerde lijst.
-
-
+for ($i = 0; $i < Count($arr); $i++){
+    echo $arr[$i] . "<br>";
+}
+echo "<br>";
 // Itereer er over met een foreach-loop. Print ze af in HTML genummerde lijst.
-
-
-
+foreach($arr as $namekey => $name){
+    echo ($namekey +1) ." " . $name;
+    echo "<br>";
+}
+echo "<br>";
 $naam = "Willy";
 
 // Itereer met een for-loop over de variable $naam alsof het een array was.
-
+for ($i = 0; $i < strlen($naam); $i++){
+    echo $naam[$i] . "<br>";
+}
 ?>
