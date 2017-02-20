231f58d99d8b03e8efedb8a18ffa47184a3c29e8
Arne <arne.vdlangenbergh@immalle.be>
2017-02-20 10:03:58 +0100

    oef 7 bezig

--- 07_switch_scrabble.php
+++ 07_switch_scrabble.php
@@ -14,6 +14,29 @@ $punten = 0;
 // R 2 punten
 // I 1 punt
 // N 1 punt
-
+for ($i = 0; $i < strlen($woord); $i++){
+    switch ($woord[$i]){
+        case q:
+        echo "Q 10 punten";
+        break;
+        case u:
+        echo "U 4 punten";
+        break;
+        case o:
+        echo "O 1 punten";
+        break;
+        case t:
+        echo "T 2 punten";
+        break;
+        case r:
+        echo "R 2 punten";
+        break;
+        case e:
+        case i:
+        case n:
+        echo $woord[$i]+ " 1 punt";
+        break;
+    }
+}
 
 ?>
